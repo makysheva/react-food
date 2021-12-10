9cb22e19eeaf8a6e6dce08712d263ccb6d80a0df
makysheva <makysheva.aliya@gmail.com>
2021-12-11 02:44:20 +0600

    логика работы переключения текста

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from 'react';
 import styles from './App.module.scss';
+import cardStyles from './components/Card/Card.module.scss';
 import Card from './components/Card';
+import cardList from './data.json';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h2 className={styles.title}>Ты сегодня покормил кота?</h2>
       <div className={styles.wrap}>
-        <Card />
+        {cardList.cardList.map((item) => {
+          return !item.isActive ? (
+            <Card item={item} key={item.id} cardDisabled={cardStyles.cardDisabled} />
+          ) : (
+            <Card item={item} key={item.id} />
+          );
+        })}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,78 +1,65 @@
 import React from 'react';
 import styles from './Card.module.scss';
 
-const Card = () => {
-  const [state, setState] = React.useState();
+const Card = ({ item, cardDisabled }) => {
+  const [clickedCard, setClickedCard] = React.useState(false);
+  const [infoCard, setInfoCard] = React.useState('Чего сидишь? Порадуй котэ, ');
+  const [cardSelectedClassName, setCardSelectedClassName] = React.useState('');
+
+  console.log(111, cardDisabled);
+  React.useEffect(() => {
+    if (!item.isActive) {
+      setInfoCard('Печалька, с курой закончился.');
+    }
+  }, []);
 
   const handleClickCard = () => {
-    console.log(`id ${this.id}`);
+    if (!item.isActive) {
+      return null;
+    } else if (clickedCard) {
+      setInfoCard('Головы щучьи с чесноком да свежайшая сёмгушка.');
+      setClickedCard(!clickedCard);
+      setCardSelectedClassName('cardSelected');
+    } else {
+      setClickedCard(!clickedCard);
+      setInfoCard('Чего сидишь? Порадуй котэ, ');
+      setCardSelectedClassName('');
+    }
   };
 
-  const cardList = [
-    {
-      id: 1,
-      slogan: 'Сказочное заморское яство',
-      name: 'Нямушка',
-      productView: 'с фуа-гра',
-      portionCount: 10,
-      gift: 'мышь в подарок',
-      isSatisfied: false,
-      packing: '0,5',
-    },
-    {
-      id: 2,
-      slogan: 'Сказочное заморское яство',
-      name: 'Нямушка',
-      productView: 'с рыбой',
-      portionCount: 40,
-      gift: '2 мыши в подарок',
-      isSatisfied: false,
-      packing: 2,
-    },
-    {
-      id: 3,
-      slogan: 'Сказочное заморское яство',
-      name: 'Нямушка',
-      productView: 'с курой',
-      portionCount: 100,
-      gift: '5 мышей в подарок',
-      isSatisfied: true,
-      packing: 5,
-    },
-  ];
-
   return (
-    <>
-      {cardList.map((item) => {
-        return (
-          <>
-            <div className={styles.box} key={item.id}>
-              <div className={styles.card} onClick={handleClickCard} id={item.id}>
-                <div className={styles.cardContent}>
-                  <p className={styles.slogan}>{item.slogan}</p>
-                  <h2 className={styles.name}>{item.name}</h2>
-                  <h3 className={styles.productView}>{item.productView}</h3>
-                  <p className={styles.portionCount}>
-                    {item.portionCount} <span className={styles.portion}>порций</span>
-                  </p>
-                  <p className={styles.gift}>{item.gift}</p>
-                  {item.isSatisfied && <p className={styles.isSatisfied}>заказчик доволен</p>}
-                </div>
-                <div className={styles.packing}>
-                  <span>{item.packing}</span> <span>кг</span>
-                </div>
-              </div>
-              <div className={styles.proposal}>
-                Чего сидишь? Порадуй котэ,{' '}
-                <a href="/" className={styles.buy}>
-                  купи.
-                </a>
-              </div>
-            </div>
-          </>
-        );
-      })}
-    </>
+    <div className={styles.box}>
+      <div
+        className={`${styles.card} ${cardDisabled ? cardDisabled : ''} ${cardSelectedClassName}`}
+        onClick={handleClickCard}
+        id={item.id}>
+        <div className={styles.cardContent}>
+          <p className={styles.slogan}>{item.slogan}</p>
+          <h2 className={styles.name}>{item.name}</h2>
+          <h3 className={styles.productView}>{item.productView}</h3>
+          <p className={styles.portionCount}>
+            {item.portionCount} <span className={styles.portion}>порций</span>
+          </p>
+          <p className={styles.gift}>{item.gift}</p>
+          {item.isSatisfied && <p className={styles.isSatisfied}>заказчик доволен</p>}
+        </div>
+        <img src="./cat.png" alt="cat" className={styles.catImg} />
+        <div className={styles.packing}>
+          <span>{item.packing}</span> <span>кг</span>
+        </div>
+      </div>
+      <div className={styles.proposal}>
+        {infoCard}
+        {clickedCard ? (
+          <a href="#" className={styles.buy} onClick={handleClickCard}>
+            {' '}
+            купи.
+          </a>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
   );
 };
 
